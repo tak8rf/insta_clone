593a2aedb1257a5939a16273278ab1a196b18a0b
Takumi Nagashima <emperte2008@gmail.com>
2022-09-12 21:32:45 +0900

    Dart修正

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -32,17 +32,16 @@
 // </div>
 
 
-import Vue from 'vue/dist/vue.esm'
+import Vue from 'vue'
 import App from '../app.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
-    el: '#hello',
-    data: {
-      message: "Can you say hello?"
-    },
-    components: { app }
-  })
+    render: h => h(App)
+  }).$mount()
+  document.body.appendChild(app.$el)
+
+  console.log(app)
 })
 
 //
